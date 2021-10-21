52e03408e3d650892d6edd64372f2afc330c7eb9
Om Gaikwad <62072438+omgaikwad@users.noreply.github.com>
2021-10-21 16:45:37 +0530

    added WelcomeScreen

--- App.js
+++ App.js
@@ -1,57 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        alignContent: "center",
-        flexWrap: "wrap",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "yellowgreen",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
---
+++ app/config/colors.js
@@ -0,0 +1,5 @@
+export default {
+  primary: "#fc5f65",
+  secondary: "#4ecdc4",
+  black: "#000",
+};
